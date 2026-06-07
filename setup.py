9227856a72b80e184783207668a0cd38c53c755e
vlxar42 <j94946108@gmail.com>
2026-06-07 21:13:15 +0100

    Add setup.py for packaging the tok3ns script

---
+++ setup.py
@@ -0,0 +1,29 @@
+# Python
+# setup.py for packaging a single script called 'tok3ns'
+
+from setuptools import setup, find_packages
+
+setup(
+    name="tok3ns",  # Name of your package
+    version="0.1.0",  # Initial version
+    description="A Python package with a single script called tok3ns",
+    author="Your Name",
+    author_email="your.email@example.com",
+    py_modules=["tok3ns"],  # Single script, no packages
+    install_requires=[
+        # List any dependencies here, e.g., "requests", "numpy"
+        "requests"
+    ],
+    entry_points={
+        "console_scripts": [
+            # Creates a command-line executable `tok3ns` that calls main() in tok3ns.py
+            "tok3ns=tok3ns:main"
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
